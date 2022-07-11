55d9be1426b8db29db80481ca9d3d6c68a364f2d
tsuyoron <108647290+tsuyoron@users.noreply.github.com>
2022-07-11 11:38:00 +0000

    [Finish] 3-4

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # .../lists/1 や .../lists/3 に該当する
+  get 'lists/:id' => 'lists#show' ,as: 'list'
 end
