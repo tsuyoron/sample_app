3b0e9f876480ebaffa97bf0f518f9f91d11e2b07
tsuyoron <108647290+tsuyoron@users.noreply.github.com>
2022-07-12 09:47:08 +0000

    [Finish] 3-11

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,7 +6,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to list_path(list.id)
+      redirect_to '/lists'
     else
       #render:indexで3-9確認
       render :new
